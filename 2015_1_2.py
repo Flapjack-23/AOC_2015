67f5d0e37b88bb922a368c07319bff2dcc75eacc
Flapjack-23 <99353088+Flapjack-23@users.noreply.github.com>
2022-02-20 14:43:12 -0600

    Add files via upload

---
+++ 2015_1_2.py
@@ -0,0 +1,15 @@
+puzzle_input = open('2015_1.txt', 'r')
+data = puzzle_input.read()
+
+floor = 0
+
+for i in range(0, len(data)):
+    if(data[i] == '('):
+        floor = floor + 1
+    if(data[i] == ')'):
+        floor = floor - 1
+    if floor == -1:
+        print(i + 1)
+
+
+print(floor)
